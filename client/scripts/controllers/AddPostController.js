deb5f6ddf098babc85b984cc846514b0b15d945d
Alex Bennett <alexandergraybennett@gmail.com>
2016-03-12 20:43:33 -0700

    adding add a collection

--- client/scripts/controllers/AddPostController.js
+++
@@ -1,6 +0,0 @@
-angular.module('capstone')
-  .controller('addpostcontroller', addpostcontroller)
-
-  function addpostcontroller($scope){
-    $scope.hello = "Hello from add post"
-  }
---
+++ client/scripts/controllers/addcollection.js
@@ -0,0 +1,7 @@
+angular.module('capstone')
+  .controller('AddCollectionController', AddCollectionController)
+
+  function AddCollectionController($scope, getUsers){
+
+    $scope.yo = "Hello from add a collection!"
+  }
